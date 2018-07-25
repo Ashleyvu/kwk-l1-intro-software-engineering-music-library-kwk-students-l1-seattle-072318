9dcda545f4dd6ee16a1bc2759e6a54d0ecd47143
Ashley Vu <ashleyvuhoo@gmail.com>
2018-07-25 20:10:03 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -12,6 +12,6 @@
 #4. This method should add a new artist and song (key value pair) to the hash and return the updated hash. 
 # This method should accept three arguments, the library hash, the artist, and the song.
 
-
-
+def list_artists 
+  
 
